80369ff860f0b11ed382878b92a9441ee7b50624
fotini <f.batzoni@gmail.com>
2023-02-26 00:15:29 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // Add this line
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header-component/header.component';
+import { FormComponent } from './form-component/form.component';
+import { FooterComponent } from './footer-component/footer.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FormComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule // Add this line
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/form-component/form.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent {
+  fullName: string;
+  email: string;
+  city: string;
+  postCode: string;
+  address: string;
+  message: string;
+  agreeTerms: boolean;
+
+  constructor() {
+    this.fullName = '';
+    this.email = '';
+    this.city = '';
+    this.postCode = '';
+    this.address = '';
+    this.message = '';
+    this.agreeTerms = false;
+  }
+
+  onSubmit() {
+    // Submit form data
+  }
+}
